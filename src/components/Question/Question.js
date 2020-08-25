1b04052544efac4aca60c0e5e8b00cd4ca493ebd
Peletskaya <ego.iriska@gmail.com>
2020-08-25 18:16:13 +0300

    create component Question add component AudioPlayer add image for placeholder

---
+++ src/components/Question/Question.js
@@ -0,0 +1,31 @@
+import React from "react";
+import AudioPlayer, { RHAP_UI } from 'react-h5-audio-player';
+import 'react-h5-audio-player/src/styles.scss';
+import '../../styles/AudioPlayer.scss'
+import './Question.scss';
+
+
+const Question = () => {
+  return (
+    <div>
+      <div className="Block_question">
+        <div  className="Placeholder" >
+          <img src="/src/assets/images/bird.jpg" alt="Plag" />
+        </div>
+        <div className="Audio">
+          <div className="Answer">
+            <h3>*******</h3>
+            <hr />
+          </div>
+          <AudioPlayer 
+            src= "https://www.xeno-canto.org/sounds/uploaded/XIQVMQVUPP/XC518684-Grands%20corbeaux%2009012020%20Suzon.mp3" 
+          />
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Question;
