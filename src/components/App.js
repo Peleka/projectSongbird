dfce2e943062643a85f44e8618372dbac62ae19e
Peletskaya <ego.iriska@gmail.com>
2020-09-01 15:20:13 +0300

    create component Description.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import "../scss/base.scss";
 import Header from "./Header/Header";
 import Question from "./Question/Question";
+import Description from "./Description/Description";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Question />
+      <div>
+        <Description />
+      </div>
     </div>
   );
 };
