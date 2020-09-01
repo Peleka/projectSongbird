c36834e1b11e090b0c0445caa5a4842d5a4f4671
Peletskaya <ego.iriska@gmail.com>
2020-09-01 15:20:46 +0300

    create component Description.js

---
+++ src/components/Description/Description.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './Description.scss';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/src/styles.scss';
+
+const Description = () => {
+  return (
+    <div className="Block_description">
+      <div className="Wrapper__flex">
+        <div className="Dird_description">
+          <img src="https://live.staticflickr.com//65535//49298804222_474cfe8682.jpg" alt="Bird" /> 
+        </div>
+        <div className="Audio">
+          <div className="Answer">
+            <h3>"Ворон"</h3>
+            <div className="Species">
+            <h3>"Corvus corax"</h3>
+            </div>
+          </div>
+            <AudioPlayer 
+              src= "https://www.xeno-canto.org/sounds/uploaded/XIQVMQVUPP/XC518684-Grands%20corbeaux%2009012020%20Suzon.mp3" 
+            />
+        </div>
+      </div>
+      <p>
+        "Ворон – крупная птица. Длина тела достигает 70 сантиметров, размах крыльев – до полутора метров. Вороны населяют окрестности Тауэра. В Англии бытует поверье, что в день, когда черные вороны улетят от Тауэра, монархия рухнет."</p>
+    </div>
+  )
+}
+
+export default Description;
