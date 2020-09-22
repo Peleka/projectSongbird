68743119480ebd51683d87a056ce24f6e9e5e4f7
Peletskaya <ego.iriska@gmail.com>
2020-09-22 21:34:08 +0300

    feat: add header logic

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,31 @@
 import React from "react";
 import "./Header.scss";
 
-const Header = () => {
+const nameRounds = ["Разминка", "Воробьиные", "Лесные птицы", "Певчие птицы", "Хищные птицы", "Морские птицы"];
+
+const Header = (props) => {
+  const listRounds = nameRounds.map((nameRound, index) =>
+ {
+  let className = ''
+  if ( index === props.currentQuestionIndex ) {
+      className = 'active'
+ }
+   return (
+     <li key={index} className={className}>
+       {nameRound}
+     </li>
+   )
+  });
+
   return (
     <div className="Header">
       <div className="Header__wrapper">
         <img className="Header__logo" src="/src/assets/images/logo.svg" alt="Logo" />
-        <h3>Score: 0</h3>
+        <h3>Score: {props.score}</h3>
       </div>
+
       <ul className="Header__rounds">
-        <li>Разминка</li>
-        <li>Воробьиные</li>
-        <li>Лесные птицы</li>
-        <li>Певчие птицы</li>
-        <li>Хищные птицы</li>
-        <li>Морские птицы</li>
+        {listRounds}
       </ul>
     </div>
   );
