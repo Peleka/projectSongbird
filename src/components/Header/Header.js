96e6f5f8bffc7e6564ff0bc6ade75862d4a714dc
Peletskaya <ego.iriska@gmail.com>
2020-09-23 09:51:15 +0300

    run prettier and remove wrong file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,40 @@
 import React from "react";
 import "./Header.scss";
 
-const nameRounds = ["Разминка", "Воробьиные", "Лесные птицы", "Певчие птицы", "Хищные птицы", "Морские птицы"];
+const nameRounds = [
+  "Разминка",
+  "Воробьиные",
+  "Лесные птицы",
+  "Певчие птицы",
+  "Хищные птицы",
+  "Морские птицы",
+];
 
 const Header = (props) => {
-  const listRounds = nameRounds.map((nameRound, index) =>
- {
-  let className = ''
-  if ( index === props.currentQuestionIndex ) {
-      className = 'active'
- }
-   return (
-     <li key={index} className={className}>
-       {nameRound}
-     </li>
-   )
+  const listRounds = nameRounds.map((nameRound, index) => {
+    let className = "";
+    if (index === props.currentQuestionIndex) {
+      className = "active";
+    }
+    return (
+      <li key={index} className={className}>
+        {nameRound}
+      </li>
+    );
   });
 
   return (
     <div className="Header">
       <div className="Header__wrapper">
-        <img className="Header__logo" src="/src/assets/images/logo.svg" alt="Logo" />
+        <img
+          className="Header__logo"
+          src="/src/assets/images/logo.svg"
+          alt="Logo"
+        />
         <h3>Score: {props.score}</h3>
       </div>
 
-      <ul className="Header__rounds">
-        {listRounds}
-      </ul>
+      <ul className="Header__rounds">{listRounds}</ul>
     </div>
   );
 };
