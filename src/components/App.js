d22fd6ca7f0cb86d09dd20036a938b6ed02831c1
Peletskaya <ego.iriska@gmail.com>
2020-10-25 11:04:33 +0300

    feat: add FinishedPage

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ const App = () => {
   const [rightAnswerId, setRightAnswerId] = useState(
     Math.ceil(Math.random() * 6)
   );
+  const [isFinished, setIsFinished] = useState(false);
   const [attemptsCount, setAttemptsCount] = useState(0);
   const [currentAnswerId, setCurrentAnswerId] = useState(null);
   const [isMarked, setIsMarked] = useState({}); // инфа о текущем клике пользователя
@@ -40,49 +41,68 @@ const App = () => {
   };
 
   const onNextLevelClickHandler = () => {
-    setCurrentRoundIndex((prevRound) => prevRound + 1);
-    setRightAnswerId(Math.ceil(Math.random() * 6));
-    setIsMarked({});
-    setCurrentAnswerId(null);
-    setAttemptsCount(0);
+    if (isStateFinished()) {
+      setIsFinished(true);
+    } else {
+      setCurrentRoundIndex((prevRound) => prevRound + 1);
+      setRightAnswerId(Math.ceil(Math.random() * 6));
+      setIsMarked({});
+      setCurrentAnswerId(null);
+      setAttemptsCount(0);
+    }
   };
+
+  const isStateFinished = () => {
+    return currentRoundIndex + 1 === birdsData[currentRoundIndex].length;
+  };
+
+  console.log("length", birdsData[currentRoundIndex].length);
 
   return (
     <div>
       <Header score={score} currentRoundIndex={currentRoundIndex} />
-      <Question
-        rightAnswer={
-          rightAnswerId
-            ? birdsData[currentRoundIndex].find(
-                (item) => item.id === rightAnswerId
-              )
-            : null
-        }
-        gotRightAnswer={gotRightAnswer}
-      />
-      <div className="Wrapper">
-        <AnswerList
-          answers={birdsData[currentRoundIndex]}
-          onAnswerClick={onAnswerClickHandler}
-          isMarked={isMarked}
-          rightAnswerId={rightAnswerId}
+      {isFinished ? (
+        <FinishPage
+          score={score}
+          onRetry={retryHandler}
+          isFinished={isFinished}
         />
-        <Description
-          selectedAnswer={
-            currentAnswerId
-              ? birdsData[currentRoundIndex].find(
-                  (item) => item.id === currentAnswerId
-                )
-              : null
-          }
-        />
-      </div>
-      <Button
-        text="NEXT LEVEL"
-        gotRightAnswer={gotRightAnswer}
-        onLevelClick={onNextLevelClickHandler}
-      />
-      <FinishPage score={score} onRetry={retryHandler} />
+      ) : (
+        <div>
+          <Question
+            rightAnswer={
+              rightAnswerId
+                ? birdsData[currentRoundIndex].find(
+                    (item) => item.id === rightAnswerId
+                  )
+                : null
+            }
+            gotRightAnswer={gotRightAnswer}
+          />
+          <div className="Wrapper">
+            <AnswerList
+              answers={birdsData[currentRoundIndex]}
+              onAnswerClick={onAnswerClickHandler}
+              isMarked={isMarked}
+              rightAnswerId={rightAnswerId}
+            />
+            <Description
+              selectedAnswer={
+                currentAnswerId
+                  ? birdsData[currentRoundIndex].find(
+                      (item) => item.id === currentAnswerId
+                    )
+                  : null
+              }
+            />
+          </div>
+          <Button
+            text="NEXT LEVEL"
+            gotRightAnswer={gotRightAnswer}
+            onLevelClick={onNextLevelClickHandler}
+          />
+        </div>
+      )}
     </div>
   );
 };
