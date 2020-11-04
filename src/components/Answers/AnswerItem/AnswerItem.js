dd7259b95d3f508f2cdaa288f65b217cfe9f6cdc
Peletskaya <ego.iriska@gmail.com>
2020-11-04 11:25:19 +0300

    work with AudioPlayer

--- src/components/Answers/AnswerItem/AnswerItem.js
+++ src/components/Answers/AnswerItem/AnswerItem.js
@@ -2,19 +2,12 @@ import React from "react";
 import "./AnswerItem.scss";
 
 const AnswerItem = (props) => {
-  const audio = new Audio();
-  const audioUrl = props.isCorrect
-    ? "../src/assets/audio/audioCorrect.mp3"
-    : "../src/assets/audio/audioIncorrect.mp3";
   const markedClass = props.isCorrect ? "correct" : "incorrect";
-
   const className = `circle ${props.isMarked ? markedClass : ""}`;
   return (
     <li
       className="AnswerItem"
       onClick={() => {
-        audio.src = audioUrl;
-        audio.play();
         props.onAnswerClick(props.answer.id);
       }}
     >
--- src/components/Answers/AnswerList.js
+++ src/components/Answers/AnswerList.js
@@ -1,19 +1,30 @@
 import React from "react";
 import "./AnswerList.scss";
 import AnswerItem from "./AnswerItem/AnswerItem";
+import AudioPlayer, { RHAP_UI } from "react-h5-audio-player";
 
 const AnswerList = (props) => {
+  const audio1 = "../src/assets/audio/audioCorrect.mp3";
+  const audio2 = "../src/assets/audio/audioIncorrect.mp3";
+
   return (
     <ul className="AnswerList">
       {props.answers.map((answer, index) => {
         return (
-          <AnswerItem
-            key={index}
-            answer={answer}
-            onAnswerClick={props.onAnswerClick}
-            isMarked={!!props.isMarked[answer.id]}
-            isCorrect={props.rightAnswerId === answer.id}
-          />
+          <div>
+            <div className="AnswerList__player">
+              <AudioPlayer
+                src={props.rightAnswerId === answer.id ? audio1 : audio2}
+              />
+            </div>
+            <AnswerItem
+              key={index}
+              answer={answer}
+              onAnswerClick={props.onAnswerClick}
+              isMarked={!!props.isMarked[answer.id]}
+              isCorrect={props.rightAnswerId === answer.id}
+            />
+          </div>
         );
       })}
     </ul>
